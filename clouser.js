6d8b6d00c7d8801ee7bf3fb306478143b3126c0a
Mohd. Jahidul Islam <jahidjob5@outlook.com>
2024-01-08 23:43:50 +0300

    lexicalScope||Closer

--- clouser.js
+++ clouser.js
@@ -2,3 +2,16 @@ console.log("clouser.js");
 
 
 
+
+function helloMain(){
+
+  const name = "ParentFunction";
+
+    function childOne(){
+        console.log(name);
+
+    }
+  return childOne;
+
+}
+console.log(helloMain())
